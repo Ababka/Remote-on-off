de4052509919047c23287abf5c7e5ea45cba843a
Anthony Babka <88292346+Ababka@users.noreply.github.com>
2022-01-12 20:07:11 -0800

    added file to repo

---
+++ A-on_D-off (2020_12_17 04_50_34 UTC).ino
@@ -0,0 +1,35 @@
+#include "IRremote.h"
+
+const int receiver = 9; // Signal Pin of IR receiver to Arduino Pin 9
+const int LedPin = 11; //output to pin 11
+int LedState = LOW;
+
+IRrecv irrecv(receiver);
+decode_results results;
+ 
+void setup(){
+  Serial.begin(9600);
+  pinMode(LedPin, OUTPUT);
+  Serial.println("IR Controls Online"); 
+  irrecv.enableIRIn(); // Start the receiver
+}
+ 
+void loop(){
+  if (irrecv.decode(&results)){ // have we received an IR signal?
+    translateIR(); 
+    irrecv.resume(); // receive the next value
+  }  
+}
+
+void translateIR(){ // takes action based on IR code received
+ switch(results.value){
+  case 0xE0E036C9: digitalWrite(LedPin, HIGH);
+    Serial.println(" A Button   Led ON"); break;
+  case 0xE0E06897: digitalWrite(LedPin, LOW);
+    Serial.println(" D button   Led OFF"); break;
+  default:
+    Serial.println(" Button not in use"); 
+
+  }
+  delay(500); // Do not get immediate repeat
+}
---
+++ button_on-off (2020_12_17 04_50_34 UTC).ino
@@ -0,0 +1,33 @@
+/*
+turn on a LED when the button is pressed and stays on 
+until the button is pressed again
+*/
+int pinButton = 4;
+int LED = 8;
+int stateLED = LOW;
+int stateButton;
+int previous = LOW;
+long time = 0;
+long debounce = 5000;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pinButton, INPUT);
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  stateButton = digitalRead(pinButton);  
+  if(stateButton == HIGH && previous == LOW && millis() - time > debounce) {
+    if(stateLED == HIGH){
+      stateLED = LOW;
+      Serial.println("OFF");
+    } else {
+       stateLED = HIGH;
+       Serial.println("ON"); 
+    }
+    time = millis();
+  }
+  digitalWrite(LED, stateLED);
+  previous == stateButton;
+}
